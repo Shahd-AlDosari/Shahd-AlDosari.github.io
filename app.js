c4dd6ecb285ca15dd336df7e56e8f6ab6477be6f
Shahd-AlDosari <shahad.r.aldosari@hotmail.com>
2021-10-18 23:40:11 +0300

    Copied Changes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-console.log("This is working");
+console.log("This is working!");
 
 (function () {
-    var myConnector = tableau.makeConnector();
+  var myConnector = tableau.makeConnector();
 
-    myConnector.getSchema = function (schemaCallback) { 
-    	const covidCols = [
+  myConnector.getSchema = function (schemaCallback) {
+    const covidCols = [
       {
         id: "Date_of_report",
         dataType: tableau.dataTypeEnum.date,
@@ -41,18 +41,40 @@ console.log("This is working");
       columns: covidCols,
     };
 
-    schemaCallback([covidTableSchema])
-};
+    schemaCallback([covidTableSchema]);
+  };
 
-myConnector.getData = function (table, doneCallback) {
+  myConnector.getData = function (table, doneCallback) {
+    let tableData = [];
+    var i = 0;
 
-    };
+    $.getJSON(
+      "https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.json",
+      function (resp) {
+        // Iterate over the JSON object
+        for (i = 0, len = resp.length; i < len; i++) {
+          tableData.push({
+            Date_of_report: resp[i].Date_of_report,
+            Municipality_code: resp[i].Municipality_code,
+            Municipality_name: resp[i].Municipality_name,
+            Province: resp[i].Province,
+            Total_reported: resp[i].Total_reported,
+            Hospital_admission: resp[i].Hospital_admission,
+            Deceased: resp[i].Deceased,
+          });
+        }
+        table.appendRows(tableData);
+        doneCallback();
+      }
+    );
+  };
 
-    tableau.registerConnector(myConnector);
+  tableau.registerConnector(myConnector);
 })();
 
-document.querySelector("#getData").addEventListner('click', getData)
-function getData (){
-	tableau.connectionName = "Dutch Corona Numbers";
-	tableau.submit()
+document.querySelector("#getData").addEventListener("click", getData);
+
+function getData() {
+  tableau.connectionName = "Dutch Corona Numbers";
+  tableau.submit();
 }
